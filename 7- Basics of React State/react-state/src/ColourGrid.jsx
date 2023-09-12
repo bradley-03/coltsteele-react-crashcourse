9ce5376293cf50ad1b74b6ef76536c1c4bbb6a42
bradley-03 <104906904+bradley-03@users.noreply.github.com>
2023-09-12 13:32:34 +0100

    Refactoring

--- 7- Basics of React State/react-state/src/ColourGrid.jsx
+++ 7- Basics of React State/react-state/src/ColourGrid.jsx
@@ -2,33 +2,13 @@ import ColourBox from "./ColourBox"
 import './ColourGrid.css'
 
 export default function ColourGrid({ colours }) {
+    const boxes = []
+    for (let i = 0; i < 25; i++) {
+        boxes.push(<ColourBox colours={colours }/>)
+    }
     return (
         <div className="ColourGrid">
-            <ColourBox colours={colours} />
-            <ColourBox colours={colours} />
-            <ColourBox colours={colours} />
-            <ColourBox colours={colours} />
-            <ColourBox colours={colours} />
-            <ColourBox colours={colours} />
-            <ColourBox colours={colours} />
-            <ColourBox colours={colours} />
-            <ColourBox colours={colours} />
-            <ColourBox colours={colours} />
-            <ColourBox colours={colours} />
-            <ColourBox colours={colours} />
-            <ColourBox colours={colours} />
-            <ColourBox colours={colours} />
-            <ColourBox colours={colours} />
-            <ColourBox colours={colours} />
-            <ColourBox colours={colours} />
-            <ColourBox colours={colours} />
-            <ColourBox colours={colours} />
-            <ColourBox colours={colours} />
-            <ColourBox colours={colours} />
-            <ColourBox colours={colours} />
-            <ColourBox colours={colours} />
-            <ColourBox colours={colours} />
-            <ColourBox colours={colours} />
+            {boxes}
         </div>
     )
 }
