72c82b11f7a1e202775e5b87fdaef8b84e804cb4
bradley-03 <104906904+bradley-03@users.noreply.github.com>
2023-09-12 13:24:20 +0100

    Refactoring

--- 7- Basics of React State/react-state/src/ColourBox.jsx
+++ 7- Basics of React State/react-state/src/ColourBox.jsx
@@ -1,9 +1,12 @@
 import './ColourBox.css'
 import { useState } from 'react'
 
-export default function ColourBox ({defaultColour, colours}) {
-    const [colour, setColour] = useState(defaultColour)
-    const newColour = () => setColour(colours[Math.floor(Math.random() * colours.length)]) 
+export default function ColourBox ({colours}) {
+    const randomColour = () => colours[Math.floor(Math.random() * colours.length)]
+
+    const [colour, setColour] = useState(randomColour)
+    const newColour = () => setColour(randomColour) 
+
     return (
         <div className="ColourBox" style={{backgroundColor: colour}} onClick={newColour} ></div>
     )
--- 7- Basics of React State/react-state/src/ColourGrid.jsx
+++ 7- Basics of React State/react-state/src/ColourGrid.jsx
@@ -2,35 +2,33 @@ import ColourBox from "./ColourBox"
 import './ColourGrid.css'
 
 export default function ColourGrid({ colours }) {
-    const randomColour = () => colours[Math.floor(Math.random() * colours.length)]
     return (
         <div className="ColourGrid">
-            <ColourBox defaultColour={randomColour} colours={colours} />
-            <ColourBox defaultColour={randomColour} colours={colours} />
-            <ColourBox defaultColour={randomColour} colours={colours} />
-            <ColourBox defaultColour={randomColour} colours={colours} />
-            <ColourBox defaultColour={randomColour} colours={colours} />
-            <ColourBox defaultColour={randomColour} colours={colours} />
-            <ColourBox defaultColour={randomColour} colours={colours} />
-            <ColourBox defaultColour={randomColour} colours={colours} />
-            <ColourBox defaultColour={randomColour} colours={colours} />
-            <ColourBox defaultColour={randomColour} colours={colours} />
-            <ColourBox defaultColour={randomColour} colours={colours} />
-            <ColourBox defaultColour={randomColour} colours={colours} />
-            <ColourBox defaultColour={randomColour} colours={colours} />
-            <ColourBox defaultColour={randomColour} colours={colours} />
-            <ColourBox defaultColour={randomColour} colours={colours} />
-            <ColourBox defaultColour={randomColour} colours={colours} />
-            <ColourBox defaultColour={randomColour} colours={colours} />
-            <ColourBox defaultColour={randomColour} colours={colours} />
-            <ColourBox defaultColour={randomColour} colours={colours} />
-            <ColourBox defaultColour={randomColour} colours={colours} />
-            <ColourBox defaultColour={randomColour} colours={colours} />
-            <ColourBox defaultColour={randomColour} colours={colours} />
-            <ColourBox defaultColour={randomColour} colours={colours} />
-            <ColourBox defaultColour={randomColour} colours={colours} />
-            <ColourBox defaultColour={randomColour} colours={colours} />
-
+            <ColourBox colours={colours} />
+            <ColourBox colours={colours} />
+            <ColourBox colours={colours} />
+            <ColourBox colours={colours} />
+            <ColourBox colours={colours} />
+            <ColourBox colours={colours} />
+            <ColourBox colours={colours} />
+            <ColourBox colours={colours} />
+            <ColourBox colours={colours} />
+            <ColourBox colours={colours} />
+            <ColourBox colours={colours} />
+            <ColourBox colours={colours} />
+            <ColourBox colours={colours} />
+            <ColourBox colours={colours} />
+            <ColourBox colours={colours} />
+            <ColourBox colours={colours} />
+            <ColourBox colours={colours} />
+            <ColourBox colours={colours} />
+            <ColourBox colours={colours} />
+            <ColourBox colours={colours} />
+            <ColourBox colours={colours} />
+            <ColourBox colours={colours} />
+            <ColourBox colours={colours} />
+            <ColourBox colours={colours} />
+            <ColourBox colours={colours} />
         </div>
     )
 }
