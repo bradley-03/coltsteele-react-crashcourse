f85c3a8bbd017953d44fdf7b348f384098997cff
Bradley <104906904+bradley-03@users.noreply.github.com>
2023-09-19 16:00:27 +0100

    synced working directory with course

--- 9-Component Design/component-design-dice-game/src/App.jsx
+++ 9-Component Design/component-design-dice-game/src/App.jsx
@@ -1,26 +1,11 @@
-import LuckyN from "./LuckyN";
-import { sum } from "./utils";
-import BoxGrid from "./BoxGrid";
-function lessThan4(dice) {
-  return sum(dice) < 4;
-}
-
-function allSameValue(dice) {
-  return dice.every((v) => v === dice[0]);
-}
+import Lucky7 from "./Lucky7";
 
 function App() {
-  return (
-    <>
-      {/* <BoxGrid /> */}
-      <LuckyN winCheck={lessThan4} title="Roll less than 4" />
-      <LuckyN
-        winCheck={allSameValue}
-        numDice={3}
-        title="Roll the same number"
-      />
-    </>
-  );
+	return (
+		<>
+			<Lucky7/>
+		</>
+	);
 }
 
 export default App;
