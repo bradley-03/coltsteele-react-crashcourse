52cb60dfc777f61700373d073ffef70457a2aef1
Bradley <104906904+bradley-03@users.noreply.github.com>
2023-09-20 16:41:19 +0100

    functions as props

--- 9-Component Design/component-design-dice-game/src/App.jsx
+++ 9-Component Design/component-design-dice-game/src/App.jsx
@@ -1,10 +1,19 @@
 import LuckyN from "./LuckyN";
+import {sum} from './utils'
+
+function lessThan4 (rolls) {
+	return sum(rolls) < 4
+}
+
+function sameNumbers (rolls) {
+	return rolls.every((v) => v === rolls[0])
+}
 
 function App() {
 	return (
 		<>
-			<LuckyN goal={9} />
-			<LuckyN numDice={3} goal={11} />
+			<LuckyN title="Same Numbers" numDice={3} winCheck={sameNumbers} />
+			<LuckyN title="Less than 4" numDice={2} winCheck={lessThan4} />
 		</>
 	);
 }
--- 9-Component Design/component-design-dice-game/src/LuckyN.jsx
+++ 9-Component Design/component-design-dice-game/src/LuckyN.jsx
@@ -1,19 +1,19 @@
 import { useState } from "react";
-import { getRolls, sum } from './utils'
+import { getRolls } from './utils'
 import Dice from "./Dice";
 import './LuckyN.css'
 
-export default function LuckyN ({numDice=2, goal=7}) {
+export default function LuckyN ({title="Dice Game", numDice=2, winCheck}) {
     const newRolls = () => getRolls(numDice)
 
     const [rolls, setRolls] = useState(newRolls)
 
     const rerollDice = () => setRolls(newRolls)
-    const isWinner = sum(rolls) === goal
+    const isWinner = winCheck(rolls)
 
     return (
         <div className="LuckyN">
-            <h1>Lucky {goal} {isWinner && "You Win!"}</h1>
+            <h1>{title} {isWinner && "| You Win!"}</h1>
             <Dice dice={rolls} />
             <button onClick={rerollDice}>Re-Roll</button>
         </div>
