ecf54d8e5a67b5191e452a01ea413928dc2ad630
bradley-03 <104906904+bradley-03@users.noreply.github.com>
2023-09-22 15:22:27 +0100

    device sync

--- 11- Effects/effects/src/App.jsx
+++ 11- Effects/effects/src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import Counter from './Counter'
+import QuoteFetcher from './QuoteFetcher'
 
 function App() {
   return (
 	<div>
-		
+		{/* <Counter/> */}
+		<QuoteFetcher />
 	</div>
   )
 }
---
+++ 11- Effects/effects/src/QuoteFetcher.jsx
@@ -0,0 +1,5 @@
+const QUOTE_URL = "https://inspo-quotes-api.herokuapp.com/quotes/random"
+
+export default function QuoteFetcher () {
+
+}
