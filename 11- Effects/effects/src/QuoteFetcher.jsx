3df45497ea21d0a3736efc463e439953a11da97a
bradley-03 <104906904+bradley-03@users.noreply.github.com>
2023-09-25 15:12:55 +0100

    quotefetcher component

--- 11- Effects/effects/src/QuoteFetcher.jsx
+++ 11- Effects/effects/src/QuoteFetcher.jsx
@@ -1,5 +1,33 @@
 const QUOTE_URL = "https://inspo-quotes-api.herokuapp.com/quotes/random"
 
-export default function QuoteFetcher () {
+import './QuoteFetcher.css'
 
+import { useState } from "react"
+import { useEffect } from "react"
+
+export default function QuoteFetcher () {
+    const [quote, setQuote] = useState({text: "", author: ""})
+
+    useEffect(() => {
+        fetchQuote()
+    }, [])
+
+    async function fetchQuote () {
+        try { 
+            const response = await fetch(QUOTE_URL)
+            const jsonResponse = await response.json()
+            setQuote(jsonResponse.quote)
+        } catch (e) {
+            console.log(e)
+            setQuote({text: "Failed to fetch quote."})
+        } 
+    }
+
+    return (
+        <div className="QuoteFetcher">
+            <h2>{quote.text}</h2>
+            <h3>{quote.author}</h3>
+            <button onClick={fetchQuote}>Get Quote</button>
+        </div>
+    )
 }
